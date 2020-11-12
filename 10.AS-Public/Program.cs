d4adf27b15e9ff008afd34bb7ee9dd5b56eef1bd
aysenurbhcvn <aysenurbhcvn@users.noreply.github.com>
2020-11-12 17:00:14 +0300

    Last Commit

---
+++ 10.AS-Public/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RectangleApplication
+{
+    class Rectangle
+    {
+        //member variables
+        public double length;   //we can access variables from the func Main
+        public double width;
+
+        public double GetArea()   //we can access this Func. without using any instance
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 14.FindMaxinArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _14.FindMaxinArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 0, min = 0, number;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write("Enter {0}. number ", i);
+                number = Convert.ToInt32(Console.ReadLine());
+                if (i == 1)
+                {
+                    max = number;
+                    min = number;
+                }
+                if (min > number)
+                { min = number; }
+                if (max < number)
+                { max = number; }
+
+            }
+
+            Console.WriteLine("Max number {0}:", max);
+            Console.WriteLine("Min number {0}:", min);
+            Console.ReadKey();
+        }
+    }
+
+}
+
